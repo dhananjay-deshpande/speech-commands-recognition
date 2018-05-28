78758a76bd8f1b41d71981e06f1bad2b6288156b
Dhananjay Deshpande <dhananjaydeshpande@Dhananjays-MBP.fios-router.home>
2018-05-28 13:29:35 -0400

    Added 2016 copyright notices

--- sample.sh
+++ sample.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This sample assumes you're already setup for using CloudML.  If this is your
+# first time with the service, start here:
+# https://cloud.google.com/ml/docs/how-tos/getting-set-up
+
 # Download Google speech commands data set from http://download.tensorflow.org/data/speech_commands_v0.01.tar.gz
 # Extract contents of archive into ./data folder
+
 # This should create a folder ./data/speech_commands_v0.01
 # Run ./data/split_data_set.py --cloud to create train, eval and test data sets
 # You will see the following files created in ./data -> train.csv, eval.csv, test.csv
